2fb9c81f5f2c819f687b65289301d1f74823640c
Dominik Schwind <dominik.schwind@pooliestudios.com>
2024-07-12 14:50:27 +0200

    Changes from Version 1.0.4
    * Renamed files will still be visible
    * Pagination on empty asset views
    * Various optical fixes

--- app/assets/app.ts
+++ app/assets/app.ts
@@ -5,7 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
 // add web components
